5df00820aa0bc3b3560a70cd4de8d0abd4bd01d8
Dody <setiawandody2003@gmail.com>
2021-12-02 15:29:38 +0700

    style: grave, booking, profile

---
+++ app/src/main/java/com/example/restgarden/ui/screen/BookingListFragment.kt
@@ -0,0 +1,37 @@
+package com.example.restgarden.ui.screen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.restgarden.databinding.FragmentBookingListBinding
+
+class BookingListFragment : Fragment() {
+  private var _binding: FragmentBookingListBinding? = null
+  private val binding get() = _binding!!
+  
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+  
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    // Inflate the layout for this fragment
+    _binding = FragmentBookingListBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+  
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    
+  }
+  
+  override fun onDestroy() {
+    super.onDestroy()
+    
+    _binding = null
+  }
+}
---
+++ app/src/main/java/com/example/restgarden/ui/screen/GraveDetailFragment.kt
@@ -0,0 +1,37 @@
+package com.example.restgarden.ui.screen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.restgarden.databinding.FragmentGraveDetailBinding
+
+class GraveDetailFragment : Fragment() {
+  private var _binding: FragmentGraveDetailBinding? = null
+  private val binding get() = _binding!!
+  
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+  
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    // Inflate the layout for this fragment
+    _binding = FragmentGraveDetailBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+  
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    
+  }
+  
+  override fun onDestroy() {
+    super.onDestroy()
+    
+    _binding = null
+  }
+}
