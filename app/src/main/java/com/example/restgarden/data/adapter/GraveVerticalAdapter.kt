0ca238eb4b6a58bbcae9af85e1272a9089ec6e1f
Dody <setiawandody2003@gmail.com>
2021-12-03 15:24:58 +0700

    feat(grave): filter for vertical recycler view

--- app/src/main/java/com/example/restgarden/data/adapter/GraveVerticalAdapter.kt
+++ app/src/main/java/com/example/restgarden/data/adapter/GraveVerticalAdapter.kt
@@ -1,14 +1,24 @@
 package com.example.restgarden.data.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.example.restgarden.R
 import com.example.restgarden.data.adapter.viewholder.GraveVerticalViewHolder
 import com.example.restgarden.data.model.Grave
 
 class GraveVerticalAdapter(private val graveList: List<Grave>) :
-  RecyclerView.Adapter<GraveVerticalViewHolder>() {
+  RecyclerView.Adapter<GraveVerticalViewHolder>(), Filterable {
+  
+  var graveFilterList = ArrayList<Grave>()
+  
+  init {
+    graveFilterList = graveList as ArrayList<Grave>
+  }
+  
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GraveVerticalViewHolder {
     val view =
       LayoutInflater.from(parent.context).inflate(R.layout.card_grave_vertical, parent, false)
@@ -16,8 +26,31 @@ class GraveVerticalAdapter(private val graveList: List<Grave>) :
   }
   
   override fun onBindViewHolder(holderHorizontal: GraveVerticalViewHolder, position: Int) {
-    holderHorizontal.bind(graveList[position])
+    holderHorizontal.bind(graveFilterList[position])
   }
   
-  override fun getItemCount(): Int = graveList.size
+  override fun getItemCount(): Int = graveFilterList.size
+  
+  override fun getFilter(): Filter {
+    return object : Filter() {
+      override fun performFiltering(constraint: CharSequence?): FilterResults {
+        val charSearch = constraint.toString()
+        graveFilterList = if (charSearch.trim().isBlank()) graveList as ArrayList<Grave>
+        else graveList.filter {
+          it.name.lowercase().contains(charSearch.lowercase()) || it.address.lowercase()
+            .contains(charSearch.lowercase())
+        } as ArrayList<Grave>
+        val filterResult = FilterResults()
+        filterResult.values = graveFilterList
+        return filterResult
+      }
+      
+      @SuppressLint("NotifyDataSetChanged")
+      override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+        graveFilterList = results?.values as ArrayList<Grave>
+        notifyDataSetChanged()
+      }
+      
+    }
+  }
 }
